0804a0f51d64f5b7c78e0cbf71db88604060d0bf
igor <igorniemiec00@gmail.com>
2025-04-08 23:02:41 +0200

    THD-002 | add Tile entity and alter game constants

--- src/main/java/org/umeboshi/thud/game/board/Board.java
+++ src/main/java/org/umeboshi/thud/game/board/Board.java
@@ -1,13 +1,23 @@
 package org.umeboshi.thud.game.board;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import static org.umeboshi.thud.game.GameConstants.TILES_TO_COORDINATES;
+
 public class Board {
-    private final int[][] board;
+    private final Tile[][] boardTiles;
 
-    public Board() {
-        this.board = new int[BoardConstants.BOARD_SIZE][BoardConstants.BOARD_SIZE];
+    public Board(Tile[][] boardTiles) {
+        this.boardTiles = boardTiles;
     }
 
-    public int[][] getBoard() {
-        return board;
+    public Tile getTile(String tileSignature) {
+        Pair<Integer, Integer> tilePosition = TILES_TO_COORDINATES.get(tileSignature);
+        return boardTiles[tilePosition.getLeft()][tilePosition.getRight()];
     }
+
+    public Tile getTile(Pair<Integer, Integer> coordinates) {
+        return boardTiles[coordinates.getLeft()][coordinates.getRight()];
+    }
+
 }
